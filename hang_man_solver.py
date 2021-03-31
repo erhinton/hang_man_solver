2b44be3634961740c2098c8b630233bbd6a34f62
erhinton <65618575+erhinton@users.noreply.github.com>
2021-03-31 11:43:54 -0400

    Added Base Code for Solver

---
+++ hang_man_solver.py
@@ -0,0 +1,67 @@
+import string
+
+class solver:
+    def __init__(self,path):
+        self.letter_count = dict.fromkeys(string.ascii_lowercase, 0)
+        self.all_words = set()
+        self.valid_words = set()
+        self.correct_guesses = []
+        self.chosen_letter = ""
+        with open(path,'r',encoding='utf-8') as f:
+            for item in f:
+                item = item.strip()
+                self.all_words.add(item.lower())
+                
+        self.best_guess = input("Enter the word the executioner has chosen " + 
+                                "with each blank as a star (*). \n")
+        
+    def word_length_check(self):
+        for word in self.all_words:
+            if len(word) == len(self.best_guess):
+                self.valid_words.add(word)
+        
+    def letter_picker(self):
+        self.letter_count = dict.fromkeys(string.ascii_lowercase, 0) # reset
+        for word in self.valid_words:
+            word = "".join(set(word)) # removes duplicate letters in valid words
+            for letter in word:
+                self.letter_count[letter] += 1
+        for letter in self.correct_guesses: # prevents repeating guesses
+            self.letter_count[letter] = 0
+        self.chosen_letter = max(self.letter_count, key=self.letter_count.get)
+        print(self.valid_words)
+        print(f"There are {len(self.valid_words)} valid words remaining.")
+        print(f"Try guessing {self.chosen_letter}")
+        self.correct_guesses.append(self.chosen_letter)
+        
+        
+    def results_of_guess(self):
+        self.best_guess = input("Enter word with correct letters and stars " +
+            "as blank spaces.")
+        wrong_words = set()
+        if self.chosen_letter in self.best_guess: # in case of success
+            list_of_indices = [i for i, value in enumerate(self.best_guess) 
+                if value == self.chosen_letter]
+            for word in self.valid_words:
+                for index in list_of_indices:
+                    if word[index] != self.chosen_letter:
+                        wrong_words.add(word)
+                    elif word.count(self.chosen_letter) > len(list_of_indices):
+                        wrong_words.add(word)
+            
+        else: # in case of failure
+            for word in self.valid_words:
+                if self.chosen_letter in word:
+                    wrong_words.add(word)
+        self.valid_words = self.valid_words.difference(wrong_words)
+        
+
+def main(path):
+    x = solver(path)
+    x.word_length_check()
+    while "*" in x.best_guess:
+        x.letter_picker()
+        x.results_of_guess()
+    
+if __name__ == "__main__":
+    main("scrabble_words.txt")
