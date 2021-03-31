357f0969a6051ccec71169a445e3151428780a55
erhinton <65618575+erhinton@users.noreply.github.com>
2021-03-31 14:47:57 -0400

    Added additional documentation

--- hang_man_solver.py
+++ hang_man_solver.py
@@ -10,7 +10,7 @@ class solver:
         all_words (set): a set that contains all words in the chosen text file
         valid_words (set): a set containing every word that could be the answer 
             to the hangman problem
-        correct_guesses (list): list of letters that were determined to be correct
+        guesses (list): list of letters that were determined to be correct
         chosen_letter (string): string that is the letter recommended to be guessed
         best_guess (string): string that contains correct letters and '*' as a blank space
         
@@ -19,7 +19,7 @@ class solver:
         self.letter_count = dict.fromkeys(string.ascii_lowercase, 0)
         self.all_words = set()
         self.valid_words = set()
-        self.correct_guesses = []
+        self.guesses = []
         self.chosen_letter = ""
         with open(path,'r',encoding='utf-8') as f:
             for item in f:
@@ -30,26 +30,52 @@ class solver:
                                 "with each blank as a star (*). \n")
         
     def word_length_check(self):
+        """ Filters out all words that are not the same length as best_guess
+        
+        Side effect:
+            All words that are the matching length are added to valid_words
+        """
+        
         for word in self.all_words:
             if len(word) == len(self.best_guess):
                 self.valid_words.add(word)
         
     def letter_picker(self):
+        """ Finds the most common letter among words that are the right length
+        
+        Side effects:
+            Prints out the number of valid words remaining, and the letter that 
+                is most frequent 
+            Append the chosen letter to guesses so that script will not guess
+                the same letter multiple times
+            
+        """
+        
         self.letter_count = dict.fromkeys(string.ascii_lowercase, 0) # reset
         for word in self.valid_words:
             word = "".join(set(word)) # removes duplicate letters in valid words
             for letter in word:
                 self.letter_count[letter] += 1
-        for letter in self.correct_guesses: # prevents repeating guesses
+        for letter in self.guesses: # prevents repeating guesses
             self.letter_count[letter] = 0
         self.chosen_letter = max(self.letter_count, key=self.letter_count.get)
-        print(self.valid_words)
+        print(self.valid_words) # debugging tool
         print(f"There are {len(self.valid_words)} valid words remaining.")
         print(f"Try guessing {self.chosen_letter}")
-        self.correct_guesses.append(self.chosen_letter)
+        self.guesses.append(self.chosen_letter)
         
         
     def results_of_guess(self):
+        """ Removes words from valid_words based on whether or not the guess was
+            correct
+        
+        Side effects:
+            If guess is successful, removes all words that do not have the
+                guessed letter at that exact index
+            If not successful, removes all words that have the guessed letter
+                in them
+        """
+        
         self.best_guess = input("Enter word with correct letters and stars " +
             "as blank spaces.")
         wrong_words = set()
@@ -71,11 +97,12 @@ class solver:
         
 
 def main(path):
-    x = solver(path)
-    x.word_length_check()
-    while "*" in x.best_guess:
-        x.letter_picker()
-        x.results_of_guess()
+    """Creates instace of solver object and runs it while there are remaining blank spaces"""
+    game = solver(path)
+    game.word_length_check()
+    while "*" in game.best_guess:
+        game.letter_picker()
+        game.results_of_guess()
     
 if __name__ == "__main__":
     main("scrabble_words.txt")
