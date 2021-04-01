deee2bea32c44183a82d841f2aee8a871f0b7471
erhinton <65618575+erhinton@users.noreply.github.com>
2021-03-31 22:59:32 -0400

    formatted ui code

--- hang_ui.py
+++ hang_ui.py
@@ -1,33 +1,36 @@
 from tkinter import * # change this to only import stuff you need
 from hang_man_solver import *
 
-# set up window
-root = Tk()
-root.title("Choose Word Length")
-root.geometry('700x500')
-
-# buliding length menu (proves options about what length word the exectioner has chosen) 
-tkvar = StringVar(root) 
-possible_lengths = range(1,17)
-length_menu = OptionMenu(root,  tkvar, *possible_lengths)
-length_menu.configure(background="white")
-length_menu.pack()
-
-
-# building submit button
-def on_submit():
-    submission = tkvar.get()
-    if submission:
-        if __name__ == "__main__":
-            main("scrabble_words.txt", int(submission))
-        else:
-            pass
-            
+class WordLengthDropDown:
+    def __init__(self,path):
+        self.path = path
     
-submit_button = Button(root, text='Submit',  command=on_submit)
-submit_button.pack()
+    def construct(self):
+        # set up window
+        root = Tk()
+        root.title("Choose Word Length")
+        root.geometry('700x500')
+        
+        # buliding length menu (proves options about what length word the exectioner has chosen) 
+        tkvar = StringVar(root) 
+        possible_lengths = range(1,17)
+        length_menu = OptionMenu(root,  tkvar, *possible_lengths)
+        length_menu.configure(background="white")
+        length_menu.pack()
+        
+        # building submit button
+        def on_submit(submission):
+            if submission:
+                main(self.path, int(submission))
+        submit_button = Button(root, text='Submit',  command= lambda: on_submit(tkvar.get()))
+        submit_button.pack()
+        
+        root.mainloop()
+        
+        
+def build_ui(path):
+    first_window = WordLengthDropDown(path)
+    first_window.construct()
 
-
-root.mainloop()
-
-
+if __name__ == "__main__":
+    build_ui("scrabble_words.txt")
